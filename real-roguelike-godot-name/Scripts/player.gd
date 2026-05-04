92f3d9e81725d990f622a620552c148098938262
JacobD55 <jacobndelia@gmail.com>
2026-05-04 12:03:10 -0400

    movement

--- real-roguelike-godot-name/Scripts/player.gd
+++ real-roguelike-godot-name/Scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-var SPEED = 300.0
+var speed = 300.0
 var screen_size
 
 
 func _ready() -> void:
-	screen_size = get_viewport()
+	screen_size = get_viewport_rect().size
 	hide()
 	
 
@@ -20,3 +20,33 @@ func _physics_process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1	
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		if velocity.x < 0:
+			$AnimatedSprite2D.rotation = PI/2
+		elif velocity.x > 0:
+			$AnimatedSprite2D.rotation = -PI/2
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = velocity.y < 0
+		$AnimatedSprite2D.rotation = 0
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_slide()
+	
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+	
