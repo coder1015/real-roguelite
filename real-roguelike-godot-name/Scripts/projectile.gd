6393c7b4859cf192bd5e5a3f20c6402f111d74f1
JacobD55 <jacobndelia@gmail.com>
2026-05-07 14:04:50 -0400

    obstacles

--- real-roguelike-godot-name/Scripts/projectile.gd
+++ real-roguelike-godot-name/Scripts/projectile.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 const SPEED: int = 300
 
+func _ready():
+	body_entered.connect(_on_body_entered)
+
 func _process(delta: float):
 	position += transform.x * SPEED * delta
 	$AnimatedSprite2D.play()
@@ -9,3 +12,7 @@ func _process(delta: float):
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free() 
+
+
+func _on_body_entered(body):
+	queue_free()
